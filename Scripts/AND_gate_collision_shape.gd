c9550fd8c4b91ad9f1fda4ed716a8015a05b666e
nooralshamayleh <150342828+nooralshamayleh@users.noreply.github.com>
2026-07-21 23:38:48 +0300

    make gates disappear on right click

--- Scripts/AND_gate_collision_shape.gd
+++ Scripts/AND_gate_collision_shape.gd
@@ -4,5 +4,5 @@ extends CollisionShape2D
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			node.queue_free()
