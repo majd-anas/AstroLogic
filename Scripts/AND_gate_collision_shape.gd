6780117876516e950cad4c21593c904269bc4695
nooralshamayleh <150342828+nooralshamayleh@users.noreply.github.com>
2026-07-21 23:29:47 +0300

    refactor code structure
    move GatesInv from player to earth scene

--- Scripts/AND_gate_collision_shape.gd
+++ Scripts/AND_gate_collision_shape.gd
@@ -1,7 +1,6 @@
 extends CollisionShape2D
 
 @onready var node: Node2D = $"../.."
-	
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
