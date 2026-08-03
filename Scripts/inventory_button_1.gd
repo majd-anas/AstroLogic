d47ad5da4b6a76681749fc090fd883ad3b4bdb01
nooralshamayleh <150342828+nooralshamayleh@users.noreply.github.com>
2026-08-03 20:00:47 +0300

    create AND side panel

---
+++ Scripts/inventory_button_1.gd
@@ -0,0 +1,13 @@
+extends Button
+
+func _on_pressed() -> void:
+	pass
+	#side_panel_content.text = "AND Gate
+	#
+	#Truth Table:
+		#x  y | f
+		#--------
+		#0  0 | 0
+		#0  1 | 0
+		#1  0 | 0
+		#1  1 | 1"
