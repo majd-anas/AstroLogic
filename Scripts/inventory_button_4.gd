a1a18189ceeef9c1fc49f3ac150e6770293a4519
nooralshamayleh <150342828+nooralshamayleh@users.noreply.github.com>
2026-08-04 17:39:34 +0300

    gates side panels done

--- Scripts/inventory_button_4.gd
+++ Scripts/inventory_button_4.gd
@@ -1,10 +1,10 @@
 extends Button
 
 @onready var side_panel: Panel = $"../../../SidePanel"
-@onready var or_side_panel: Panel = $"../../../SidePanel/ORSidePanel"
+@onready var inverter_side_panel: Panel = $"../../../SidePanel/InverterSidePanel"
 
 func _on_pressed() -> void:
 	var children  = side_panel.get_children()
 	for c in children:
 		c.visible = false
-	or_side_panel.visible = true
+	inverter_side_panel.visible = true
--- Scripts/side_panel.gd
+++ Scripts/side_panel.gd
@@ -1,7 +1,6 @@
 extends Panel
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var children  = get_children()
 	for c in children:
